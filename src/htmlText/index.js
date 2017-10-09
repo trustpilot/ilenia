603848edb5ac3d5922bcd973084b22ed3d114b0b
Paulo Cunha <pc@paulocunha.me>
2017-10-09 15:51:34 +0200

    Added HtmlText component

---
+++ src/htmlText/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { withTranslations } from '../';
+import sanitizeHtml from 'sanitize-html';
+
+const HtmlText = ({ id, interpolations = [], translations, key = '[HTML]' }) => {
+  let string = translations[id];
+
+  interpolations.forEach((html) => {
+    html = sanitizeHtml(html);
+  });
+
+  const stringArray = string.split(key);
+
+  if (stringArray.length <= 0) {
+    return string;
+  } else {
+    stringArray.forEach((part, index) => {
+      stringArray[index] += (interpolations[index] || '');
+    });
+  }
+
+  string = ''.concat(...stringArray);
+  return (<span dangerouslySetInnerHTML={ {__html: string} }></span>);
+};
+
+export default withTranslations(HtmlText);
