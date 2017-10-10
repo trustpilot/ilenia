351e2702dfba62adaa2274a0bac4465de481abe6
Paulo Cunha <pc@paulocunha.me>
2017-10-10 16:14:13 +0200

    fixed tests in htmlText

--- src/htmlText/__tests__/htmlText.test.js
+++ src/htmlText/__tests__/htmlText.test.js
@@ -4,14 +4,14 @@ import { LocalizationProvider, HtmlText } from '../../';
 
 const translations = {
   'test1': 'Just a random string',
-  'test2': 'A string but with a [HTML] in it',
-  'test3': 'A string that is sort of [HTML]italic[HTML] and [HTML]bold[HTML] and such',
-  'test4': 'A string that is sort of [HTML]italic[HTML]',
-  'test5': 'A string that uses {smtg}another{smtg} key'
+  'test2': 'A string but with {html} in it',
+  'test3': 'A string that is sort of {html1}italic{html2} and {html3}bold{html4} and such',
+  'test4': 'A string that is sort of {html1}italic{html2}',
+  'test5': 'A string that uses [smtg1]another[smtg2] placeholder'
 };
 
-const setupText = (string, interpolations, key) => (<LocalizationProvider locale="en-US" translations={translations}>
-  <HtmlText id={string} interpolations={interpolations} key={key} />
+const setupText = (string, interpolations, tag) => (<LocalizationProvider locale="en-US" translations={translations}>
+  <HtmlText id={string} interpolations={interpolations} tag={tag} />
 </LocalizationProvider>);
 
 test('Renders a plain string', () => {
@@ -22,28 +22,42 @@ test('Renders a plain string', () => {
 });
 
 test('Renders a string with one HTML part', () => {
-  const component = renderer.create(setupText('test2', ['<span>hello</span>']));
+  const component = renderer.create(setupText('test2', {html: '<span>hello</span>'} ));
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test('Renders a string with multiple HTML parts in it', () => {
-  const component = renderer.create(setupText('test3', ['<em>', '</em>', '<b>', '</b>']));
+  const component = renderer.create(setupText('test3', {
+    html1: '<em>',
+    html2: '</em>',
+    html3: '<b>',
+    html4: '</b>'
+  }));
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test('Renders a string with HTML parts in the end', () => {
-  const component = renderer.create(setupText('test4', ['<em>', '</em>']));
+  const component = renderer.create(setupText('test4', {
+    html1: '<em>',
+    html2: '</em>'
+  }));
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test('Renders a string with HTML marked with another key', () => {
-  const component = renderer.create(setupText('test5', ['<b>', '</b>'], '{smtg}'));
+  const component = renderer.create(setupText('test5', {
+    smtg1: '<b>',
+    smtg2: '</b>'
+  }, {
+    start: '[',
+    end: ']'
+  }));
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
