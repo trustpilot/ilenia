0acb1a5f99fb4867d3740016bc48447f192d4055
Paulo Cunha <pc@paulocunha.me>
2017-10-10 11:05:13 +0200

    string replacement matching all placeholders

--- src/text/index.js
+++ src/text/index.js
@@ -3,9 +3,14 @@ import { withTranslations } from '../';
 const Text = ({ id, interpolations = {}, translations, tag = {start: '{', end: '}'} }) => {
   let string = translations[id];
 
+  const escapeRegex = function (str) {
+    return str.replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1');
+  };
+
   for (const key in interpolations) {
     const wrappedKey = `${tag.start}${key}${tag.end}`;
-    string = string.replace(wrappedKey, interpolations[key]);
+    const re = new RegExp(escapeRegex(wrappedKey), 'g');
+    string = string.replace(re, interpolations[key]);
   }
 
   return string;
