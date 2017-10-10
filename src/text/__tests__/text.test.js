90e5b8b1bb4c7c67f5b16e5b3d0f36fa2bbfd9d7
Paulo Cunha <pc@paulocunha.me>
2017-10-10 16:18:03 +0200

    Added test for different placeholder syntax in Text

--- src/text/__tests__/text.test.js
+++ src/text/__tests__/text.test.js
@@ -5,11 +5,11 @@ import { LocalizationProvider, Text } from '../../';
 const translations = {
   'test1': 'Just a random string',
   'test2': 'A string but with a {value} in it',
-  'test3': 'A string that is sort of <b>bold</b>'
+  'test3': 'A string that uses another [placeholder]'
 };
 
-const setupText = (string, interpolations) => (<LocalizationProvider locale="en-US" translations={translations}>
-  <Text id={string} interpolations={interpolations}/>
+const setupText = (string, interpolations, tag) => (<LocalizationProvider locale="en-US" translations={translations}>
+  <Text id={string} interpolations={interpolations} tag={tag}/>
 </LocalizationProvider>);
 
 test('Renders a plain string', () => {
@@ -26,8 +26,8 @@ test('Renders a string with an interpolations', () => {
   expect(tree).toMatchSnapshot();
 });
 
-test('Renders a string with HTML in it', () => {
-  const component = renderer.create(setupText('test3'));
+test('Renders a string which uses a different placeholder syntax', () => {
+  const component = renderer.create(setupText('test3', { 'placeholder': 'syntax'}, {start: '[', end: ']'}));
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
