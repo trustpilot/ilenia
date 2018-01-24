530d27e6cb7e5f0c6723f1f2766c17a672f3b2e8
Mads Godvin Jensen <mje@trustpilot.com>
2018-01-24 16:41:21 +0100

    Move provider and create tests for it.

---
+++ src/components/localizationProvider/__tests__/localizationProvider.test.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import TestUtils from 'react-dom/test-utils';
+import LocalizationProvider from '../';
+
+const locale = 'da-DK';
+
+const translations = {
+  danish: 'Dansk',
+};
+
+const fallbackTranslations = {
+  english: 'English (US)',
+};
+
+class Child extends Component {
+  render() {
+    return <div/>;
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  static contextTypes = {
+    locale: PropTypes.string.isRequired,
+    translations: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  }
+}
+
+test('Provider merges translations and fallback translations correctly', () => {
+
+  const component = TestUtils.renderIntoDocument(
+    <LocalizationProvider locale={locale} translations={translations} fallbackTranslations={fallbackTranslations}>
+      <Child/>
+    </LocalizationProvider>);
+
+  const child = TestUtils.findRenderedComponentWithType(component, Child);
+  expect(child.context.translations.danish).toBe('Dansk');
+  expect(child.context.translations.english).toBe('English (US)');
+});
