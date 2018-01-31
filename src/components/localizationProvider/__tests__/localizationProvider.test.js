f165b345038ff5ba3286ee5a9a8ab41cd1df467d
Mads Godvin Jensen <mje@trustpilot.com>
2018-01-31 16:12:35 +0100

    Get rid of annoying warning about missing requestAnimationFrame polyfill

--- src/components/localizationProvider/__tests__/localizationProvider.test.js
+++ src/components/localizationProvider/__tests__/localizationProvider.test.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import 'raf/polyfill';
 import TestUtils from 'react-dom/test-utils';
 import LocalizationProvider from '../';
 
