2c2a2422caf11ebd296f1056a94169cc9d2e570c
Thomas Thiebaud <thiebaud.tom@gmail.com>
2018-07-11 10:07:14 +0200

    Add TimeagoText component

---
+++ src/TimeagoText.js
@@ -0,0 +1,43 @@
+import withTranslations from './withTranslations';
+import PropTypes from 'prop-types';
+import timeago from 'timeago.js';
+
+import da from 'timeago.js/locales/da';
+import de from 'timeago.js/locales/de';
+import en from 'timeago.js/locales/en';
+import es from 'timeago.js/locales/es';
+import fr from 'timeago.js/locales/fr';
+import it from 'timeago.js/locales/it';
+import nl from 'timeago.js/locales/nl';
+import sv from 'timeago.js/locales/sv';
+
+timeago.register('da-DK', da);
+timeago.register('de-DE', de);
+timeago.register('en-GB', en);
+timeago.register('en-US', en);
+timeago.register('es-ES', es);
+timeago.register('fr-FR', fr);
+timeago.register('it-IT', it);
+timeago.register('nl-NL', nl);
+timeago.register('sv-SE', sv);
+
+const TimeagoText = ({ date, locale }) => {
+  const dateWrapper = new Date(date);
+  const timeagoInstance = timeago();
+
+  if (isNaN(dateWrapper)) {
+    return null;
+  }
+
+  return timeagoInstance.format(date, locale);
+};
+
+TimeagoText.propTypes = {
+  date: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.instanceOf(Date),
+  ]),
+};
+
+export default withTranslations(TimeagoText);
---
+++ src/__tests__/timeagoText.test.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import LocalizationProvider from '../LocalizationProvider';
+import TimeagoText from '../TimeagoText';
+
+test('Renders a localized date from a number', () => {
+  const component = renderer.create(
+    <LocalizationProvider locale="en-US" translations={{}}>
+      <TimeagoText date={Date.now() - 11 * 1000 * 60 * 60} />
+    </LocalizationProvider>
+  );
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Renders a localized date from a string', () => {
+  const component = renderer.create(
+    <LocalizationProvider locale="es-ES" translations={{}}>
+      <TimeagoText date={new Date(Date.now() - 11 * 1000 * 60 * 60).toString()} />
+    </LocalizationProvider>
+  );
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Renders a localized date from a date', () => {
+  const component = renderer.create(
+    <LocalizationProvider locale="fr-FR" translations={{}}>
+      <TimeagoText date={new Date(Date.now() - 11 * 1000 * 60 * 60)} />
+    </LocalizationProvider>
+  );
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Throw an error if the date is invalid', () => {
+  const component = renderer.create(
+    <LocalizationProvider locale="en-US" translations={{}}>
+      <TimeagoText date={'abcdefghijklmnopqrstuvwxyz'} />
+    </LocalizationProvider>
+  );
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Renders a localized date with an incorrect locale', () => {
+  const component = renderer.create(
+    <LocalizationProvider locale="abcdefghijklmnopqrstuvwxyz" translations={{}}>
+      <TimeagoText date={new Date(Date.now() - 11 * 1000 * 60 * 60)} />
+    </LocalizationProvider>
+  );
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
