6ab0778becbb17424a19353115e21193856669f4
Toby Moore <tmo@trustpilot.com>
2018-07-27 13:25:09 +0200

    Update: Allow components as interpolation values

--- src/__tests__/interpolate.test.js
+++ src/__tests__/interpolate.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import interpolate from '../interpolate';
 
 test('It replaces a token in a string', () => {
@@ -31,4 +32,55 @@ test('Handles an empty input', () => {
 
   output = interpolate();
   expect(output).toBe('');
+
+  output = interpolate('nothing to do');
+  expect(output).toBe('nothing to do');
+});
+
+test('It replaces a token in a string with a component', () => {
+  const tag = (<number>1</number>);
+  const output = interpolate('This string has {number} token', { number: tag });
+
+  expect(output).toEqual(['This string has ', (<number>1</number>), ' token']);
+});
+
+test('It replaces multiple tokens in a string with components', () => {
+  const interpolations = { number: (<number>2</number>), things: (<strong>things</strong>) };
+  const output = interpolate('This string has {number} {things}', interpolations);
+
+  expect(output).toEqual(['This string has ', (<number>2</number>), ' ', (<strong>things</strong>)]);
+});
+
+test('It collapses text interpolations when there are also components', () => {
+  const interpolations = { review: 'hahaha', number: (<number>2</number>) };
+  const output = interpolate('This {review} has {number} tokens', interpolations);
+
+  expect(output).toEqual([
+    'This hahaha has ',
+    (<number>2</number>),
+    ' tokens']);
+});
+
+test('It uses a defined token tag with components', () => {
+  const tags = {
+    start: '[',
+    end: ']',
+  };
+
+  const number = (<number>1</number>);
+  const output = interpolate('This string has [number] token', { number: number }, tags);
+  expect(output).toEqual(['This string has ', (<number>1</number>), ' token']);
+});
+
+test('Doesn\'t recursively substitute', () => {
+  const interpolations = {
+    one: '{one}',
+    two: '{one}',
+    three: '{more}',
+    more: 'HAHA',
+  };
+
+  const output = interpolate('{one} {two} {three} {more} {three} {two} {one}', interpolations);
+
+  expect(output).toBe('{one} {one} {more} HAHA {more} {one} {one}');
 });
