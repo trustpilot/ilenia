2e8b3b754e6713d33afffa5928e572c40b778a17
Paulo Cunha <pac@trustpilot.com>
2019-02-05 16:56:04 +0100

    Fix: Emit error when key is missing
    When React components are used as interpolation values,
    they must have a key.

--- src/interpolate.js
+++ src/interpolate.js
@@ -1,20 +1,25 @@
-const escapeRegex = str => str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
-const replaceRegex = (tag, key) =>
-  new RegExp(escapeRegex(`${tag.start}${key}${tag.end}`), "g");
+import React from 'react';
+const escapeRegex = (str) => str.replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1');
+const replaceRegex = (tag, key) => new RegExp(escapeRegex(`${tag.start}${key}${tag.end}`), 'g');
+
+const isCastableToString = (element) => {
+  return typeof element === 'string' || typeof element === 'number';
+};
 
 const combineStrings = ([a, ...rest], s) =>
-  (typeof s === "string" || typeof s === "number") &&
-  (typeof a === "string" || typeof a === "number")
-    ? [s + a, ...rest]
-    : [s, a, ...rest];
+  isCastableToString(s) && isCastableToString(a) ? [s + a, ...rest] : [s, a, ...rest];
 
-const isNotAnEmptyString = s => typeof s !== "string" || s.length > 0;
+const isNotAnEmptyString = (s) => typeof s !== 'string' || s.length > 0;
 
-export default (
-  string = "",
-  interpolations = {},
-  tag = { start: "[", end: "]" }
-) => {
+const checkComponentForKey = (component) => {
+  if (React.isValidElement(component) && !component.key) {
+    console.error(
+      'When you define a React component or HTML element as the value of an interpolation, you need to give it a unique `key` prop.'
+    );
+  }
+};
+
+export default (string = '', interpolations = {}, tag = { start: '[', end: ']' }) => {
   function* replace(interpolationKeys, input) {
     if (interpolationKeys.length === 0) {
       yield input;
@@ -26,6 +31,7 @@ export default (
 
     yield* replace(remaining, pieces.shift());
     while (pieces.length > 0) {
+      checkComponentForKey(interpolations[key]);
       yield interpolations[key];
       yield* replace(remaining, pieces.shift());
     }
@@ -34,6 +40,6 @@ export default (
   const keys = Object.keys(interpolations);
   const segments = replace(keys, string);
   return Array.from(segments)
-    .reduceRight(combineStrings, [""])
+    .reduceRight(combineStrings, [''])
     .filter(isNotAnEmptyString);
 };
