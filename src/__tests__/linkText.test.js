d73a1f08456c74271659058424eecb21d236dae9
Paulo Cunha <pc@pmrcunha.com>
2019-02-18 15:02:05 +0100

    Build: Fix tests in linkText

--- src/__tests__/linkText.test.js
+++ src/__tests__/linkText.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import 'raf/polyfill';
-import { render, Simulate } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 import LocalizationProvider from '../LocalizationProvider';
 import LinkText from '../LinkText';
 
@@ -11,20 +10,24 @@ const translations = {
   test4: 'A link [LINK-BEGIN]in the[LINK-END] middle',
 };
 
-const setupLink = (string, links) => (
-  <LocalizationProvider locale="en-US" translations={translations}>
-    <LinkText id={string} links={links} translations={translations} />
-  </LocalizationProvider>
-);
-
 test('Renders a string with no links in it', () => {
-  const { container } = render(setupLink('test1', []));
-  expect(container.firstChild).toMatchSnapshot();
+  const { asFragment } = render(
+    <LocalizationProvider locale="en-US" translations={translations}>
+      <LinkText id="test1" links={[]} translations={translations} />
+    </LocalizationProvider>
+  );
+
+  expect(asFragment()).toMatchSnapshot();
 });
 
 test('Renders a string with one link in the middle', () => {
-  const { container } = render(setupLink('test2', [{ href: 'http://something' }]));
-  expect(container.firstChild).toMatchSnapshot();
+  const { asFragment } = render(
+    <LocalizationProvider locale="en-US" translations={translations}>
+      <LinkText id="test2" links={[{ href: 'http://something' }]} translations={translations} />
+    </LocalizationProvider>
+  );
+
+  expect(asFragment()).toMatchSnapshot();
 });
 
 test('Renders a string with multiple links in it', () => {
@@ -39,8 +42,12 @@ test('Renders a string with multiple links in it', () => {
     },
   ];
 
-  const { container } = render(setupLink('test3', links));
-  expect(container.firstChild).toMatchSnapshot();
+  const { asFragment } = render(
+    <LocalizationProvider locale="en-US" translations={translations}>
+      <LinkText id="test3" links={links} translations={translations} />
+    </LocalizationProvider>
+  );
+  expect(asFragment()).toMatchSnapshot();
 });
 
 test('Renders a string with a link with arbitrary attributes', () => {
@@ -52,14 +59,15 @@ test('Renders a string with a link with arbitrary attributes', () => {
     },
   ];
 
-  const { container } = render(setupLink('test4', links));
-  expect(container.firstChild).toMatchSnapshot();
+  const { asFragment } = render(
+    <LocalizationProvider locale="en-US" translations={translations}>
+      <LinkText id="test4" links={links} translations={translations} />
+    </LocalizationProvider>
+  );
+  expect(asFragment()).toMatchSnapshot();
 });
 
 test('Renders a string with a working onClick callback', () => {
-  // console.error node_modules/fbjs/lib/warning.js:33
-  // Warning: Detected multiple renderers concurrently rendering the same context provider. This is currently unsupported.
-
   const onClickSpy = jest.fn();
 
   const links = [
@@ -71,17 +79,26 @@ test('Renders a string with a working onClick callback', () => {
     },
   ];
 
-  const { getByText, container } = render(setupLink('test4', links));
-  Simulate.click(getByText('in the'));
-  expect(onClickSpy.mock.calls.length).toBe(1);
+  const { getByText, asFragment } = render(
+    <LocalizationProvider locale="en-US" translations={translations}>
+      <LinkText id="test4" links={links} translations={translations} />
+    </LocalizationProvider>
+  );
 
-  expect(container.firstChild).toMatchSnapshot();
+  fireEvent.click(getByText(/in the/i));
+
+  expect(onClickSpy).toHaveBeenCalledTimes(1);
+  expect(asFragment()).toMatchSnapshot();
 });
 
 test('Renders nothing when the string is not there', () => {
   console.error = jest.fn(); // eslint-disable-line no-console
-  const { container } = render(setupLink('not-there'));
+  const { asFragment } = render(
+    <LocalizationProvider locale="en-US" translations={translations}>
+      <LinkText id="not-there" translations={translations} />
+    </LocalizationProvider>
+  );
 
-  expect(container.firstChild).toMatchSnapshot();
+  expect(asFragment()).toMatchSnapshot();
   expect(console.error).toHaveBeenCalled(); // eslint-disable-line no-console
 });
