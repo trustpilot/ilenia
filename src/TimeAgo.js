049a9fd103ecea0a28b631815b1d1221eacc0464
Fernando Nahuel Nicolosi <fernicolosi@gmail.com>
2019-03-25 14:56:48 +0100

    Update: Swap time ago component with new library

--- src/TimeAgo.js
+++ src/TimeAgo.js
@@ -1,37 +1,39 @@
+import React from 'react';
 import withTranslations from './withTranslations';
 import PropTypes from 'prop-types';
-import timeago from 'timeago.js';
+import JavascriptTimeAgo from 'javascript-time-ago';
+import ReactTimeAgo from 'react-time-ago';
 
-import da from 'timeago.js/locales/da';
-import de from 'timeago.js/locales/de';
-import en from 'timeago.js/locales/en';
-import es from 'timeago.js/locales/es';
-import fr from 'timeago.js/locales/fr';
-import it from 'timeago.js/locales/it';
-import nl from 'timeago.js/locales/nl';
-import sv from 'timeago.js/locales/sv';
+import da from 'javascript-time-ago/locale/da';
+import de from 'javascript-time-ago/locale/de';
+import en from 'javascript-time-ago/locale/en';
+import es from 'javascript-time-ago/locale/es';
+import fr from 'javascript-time-ago/locale/fr';
+import it from 'javascript-time-ago/locale/it';
+import nl from 'javascript-time-ago/locale/nl';
+import sv from 'javascript-time-ago/locale/sv';
 
-timeago.register('da-DK', da);
-timeago.register('de-DE', de);
-timeago.register('en-GB', en);
-timeago.register('en-US', en);
-timeago.register('es-ES', es);
-timeago.register('fr-FR', fr);
-timeago.register('it-IT', it);
-timeago.register('nl-NL', nl);
-timeago.register('sv-SE', sv);
+JavascriptTimeAgo.locale(da);
+JavascriptTimeAgo.locale(de);
+JavascriptTimeAgo.locale(en);
+JavascriptTimeAgo.locale(en);
+JavascriptTimeAgo.locale(es);
+JavascriptTimeAgo.locale(fr);
+JavascriptTimeAgo.locale(it);
+JavascriptTimeAgo.locale(nl);
+JavascriptTimeAgo.locale(sv);
 
 const TimeAgo = ({ date, locale }) => {
-  const dateWrapper = new Date(date);
-  const timeagoInstance = timeago();
+  const dateObject = new Date(date);
+  const [language] = locale.split('-');
 
-  if (isNaN(dateWrapper)) {
+  if (isNaN(dateObject)) {
     // eslint-disable-next-line no-console
     console.error('Invalid date');
     return null;
   }
 
-  return timeagoInstance.format(date, locale);
+  return <ReactTimeAgo date={dateObject} locale={language} />;
 };
 
 TimeAgo.propTypes = {
