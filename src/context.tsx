dd6b986a967e33f0ed1bd841f09ee941a9e19fc9
Thomas Thiebaud <thiebaud.tom@gmail.com>
2019-06-24 15:46:00 +0200

    Fix: Use esModuleInterop

--- src/context.tsx
+++ src/context.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 export interface Translations {
   [keyof: string]: string;
--- src/interpolate.tsx
+++ src/interpolate.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { Interpolations, Tag } from './text';
 
 function escapeRegex(str: string) {
